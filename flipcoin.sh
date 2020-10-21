95fcf9ff22d8a03b982de199474b101bd625c80f
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-10-21 14:56:47 +0530

    added:uc2 uc3 uc4

--- flipcoin.sh
+++ flipcoin.sh
@@ -9,3 +9,51 @@ then
 else
 	echo tails winner
 fi
+#uc2
+#! /bin/bash
+declare -A Combination
+Combination=(["h"]=0 ["t"]=0)
+coin=0
+no_of_flips=21
+flip=0
+hcount=21
+tcount=0
+function head_or_tails()
+{
+        if [[ $((RANDOM%2)) -eq 1 ]]
+        then
+                coin=heads
+        else
+                coin=tails
+        fi
+}
+function UC2andUC3andUC4()
+{
+        for (( i=0; i<$no_of_flips; i++ ))
+        do
+                head_or_tails
+                input=$coin
+                if [[ $input == heads ]]
+                then
+                        Combination[h]=$(( ${Combination[h]}+1 ))
+
+                else
+                        Combination[t]=$(( ${Combination[t]}+1 ))
+
+                fi
+                                        if [[  ${Combination[h]} == ${Combination[t]} ]]
+                                   then
+                                                echo "flip coin combination tie"
+                                        continue 2
+
+                                fi
+       done
+        echo ${Combination[h]}
+        echo ${Combination[t]}
+        win_by=$(( ${Combination[h]}-${Combination[t]} ))
+        echo "win by "$win_by
+
+}
+UC2andUC3andUC4
+
+
